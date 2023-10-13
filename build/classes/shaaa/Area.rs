a54892ff74e169d492b932acd08483945734f9f5
devshaaa <shnjnmrgll@gmail.com>
2023-10-13 15:07:04 +0800

    First Commit

---
+++ build/classes/shaaa/Area.rs
@@ -0,0 +1,2 @@
+Area$Main
+Area
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[]args){
+        BankAccount account= new BankAccount("Shaa", 23, 697076279, 160.0);
+        
+        account.deposit(200.0);
+       account.deposit(310.0);
+
+        
+        System.out.println(account.getSummary());
+    }
+}
